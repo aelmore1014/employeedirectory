688fceba4531d7e026cc78a383770e13b7f703da
Alex Elmore <alexelmore1014@gmail.com>
2020-03-30 04:22:53 -0400

    Fixed bugs final commit

--- src/app.js
+++ src/app.js
@@ -1,21 +1,13 @@
-import React from 'react';
-// import logo from './logo.svg';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import './App.css';
-import Footer from "./components/Footer";
-import Home from "./pages/home";
-import Wrapper from "./components/Wrapper";
+import React from "react";
+import Container from "./components/container";
+
+
+
+
 function App() {
   return (
-    <Router>
-      <div>
-    
-        <Wrapper>
-          <Route exact path="/" component={Home} />
-        </Wrapper>
-        <Footer />
-      </div>
-    </Router>
+  
+    <Container />
   );
 }
 
